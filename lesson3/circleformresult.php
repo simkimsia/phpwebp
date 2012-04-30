9e5644946f656db2b40d40ea69234e95c8b2a59f
kimsia <kimcity@gmail.com>
2012-04-30 10:29:27 +0800

    Add in all the necessary exercises

--- lesson3/circleformresult.php
+++ lesson3/circleformresult.php
@@ -28,6 +28,8 @@
 	$area = $constantPI * $radius * $radius;
 	$perimeter = 2 * $constantPI * $radius;
 	
+	
+	$newTitle = 'Circle - Radius:' . $radius . 'cm Area:' . $area . 'cm square Perimeter:' . $perimeter . 'cm';
 	/**
 	* 
 	* ALWAYS do the html display below the logic codes
@@ -41,7 +43,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>
-			Circle stats are:
+			<?php echo $newTitle; ?>
 		</title>
 		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
 	</head>
