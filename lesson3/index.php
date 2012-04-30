b21d3b506d9106029ba68a2e62336f59a9383a97
kimsia <kimcity@gmail.com>
2012-04-30 10:31:03 +0800

    Add in new files for lesson3

---
+++ lesson3/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			Lesson 3 examples
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		<ul>
+			<li><a href="circleform.php">Circle Example</a></li>
+			<li><a href="squareform.php">Square Example</a></li>
+			<li><a href="triangleform.php">Triangle Example</a></li>						
+		</ul>
+	</body>
+</html>
---
+++ lesson3/squareformresult.php
@@ -0,0 +1,54 @@
+<?php
+
+	/**
+	* 
+	* ALWAYS do the logic codes above the html display
+	*
+	**/
+	
+	// Set default radius
+	$length = 0;
+	
+	
+	// check if user arrives here via a POSTBACK
+	if (!empty($_POST)) {
+
+		// check for existent of $_POST['radius']
+		if (!empty($_POST['length'])) {
+			
+			$length = $_POST['length'];
+			
+		} // end if $_POST['length'] is NOT empty
+		
+	} // end if user arrives here via a POSTBACK
+	
+	
+	// calculate the area and perimeter
+	$area = $length * $length;
+	$perimeter = 4 * $length;
+	
+	
+	$newTitle = 'Square - Length:' . $length . 'cm Area:' . $area . 'cm square Perimeter:' . $perimeter . 'cm';
+	/**
+	* 
+	* ALWAYS do the html display below the logic codes
+	*
+	**/
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			<?php echo $newTitle; ?>
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		Length : <?php echo $radius; ?> cm<br />
+		Area : <?php echo $area; ?> cm square<br />
+		Perimeter : <?php echo $perimeter; ?> cm<br />
+	</body>
+</html>
