0360c3a5c657c2a1114b6e258e9f910fbd87f5c2
kimsia <kimcity@gmail.com>
2012-05-02 09:08:18 +0800

    Add in lesson4 folder

---
+++ lesson4/2x3_tables_in_html.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			2 rows 3 cols Tables in HTML
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		<!-- table -->
+		<table border="1">
+			<!-- row 1 -->
+			<tr>
+				<td>row 1, col 1</td>
+				<td>row 1, col 2</td>
+				<td>row 1, col 3</td>								
+			</tr>
+			
+			<!-- row 2 -->
+			<tr>
+				<td>row 2, col 1</td>
+				<td>row 2, col 2</td>
+				<td>row 2, col 3</td>								
+			</tr>
+			
+		</table>
+	</body>
+</html>
---
+++ lesson4/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			Lesson 3 examples
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		<ul>
+			<li><a href="2x3_tables_in_html.php">2 rows 3 cols Table in HTML Example</a></li>
+			<li><a href="2x3_tables_in_php.php">2 rows 3 cols Table in PHP Example</a></li>
+			<li><a href="3x4_tables_in_php.php">3 rows 4 cols Table in PHP Example</a></li>
+			<li><a href="arrays.php">Arrays</a></li>
+			
+		</ul>
+	</body>
+</html>
