79fbc45e80b973156bf36964aba7d9c70f74612d
kimsia <kimcity@gmail.com>
2012-05-04 20:09:04 +0800

    add in github links to the index.php

--- lesson5/index.php
+++ lesson5/index.php
@@ -1,3 +1,8 @@
+<?php
+
+$githuburl = "https://github.com/simkimsia/phpwebp/blob/lesson5/lesson5/";
+
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
 "http://www.w3.org/TR/html4/strict.dtd">
 <html lang="en">
@@ -11,12 +16,12 @@
 	<body>
 		<h1>How to do Validation</h1>
 		<ul>
-			<li><a href="circle/circleform.php">Validate for number using Circle Form as example (code available at )</a></li>
-			<li><a href="square/squareform.php">Validate for compulsory textfield + number using Square Form as example (code available at )</a></li>
-			<li><a href="triangle/triangleform.php">Validate for compulsory textfield and number using Triangle Form as example (TODO)</a></li>
-			<li><a href="circle1/circleform.php">Validate for number using Circle Form as example (separate validation logic)</a></li>
-			<li><a href="register/registerform.php">Validate for compulsory fields using Register Form as example(code available at )</a></li>
-			<li><a href="Ex1-5/index.php">Ex 1-5 (TODO)</a></li>			
+			<li><a href="circle/circleform.php">Validate for number using Circle Form as example</a>  (code available <a href="<?php echo $githuburl; ?>circle">here</a>)</li>
+			<li><a href="square/squareform.php">Validate for compulsory textfield + number using Square Form as example</a>  (code available <a href="<?php echo $githuburl; ?>square">here</a>)</li>
+			<li><a href="triangle/triangleform.php">Validate for compulsory textfield and number using Triangle Form as example (TODO)</a>  (skeleton NOT available)</li>
+			<li><a href="circle1/circleform.php">Validate for number using Circle Form as example (separate validation logic)</a>  (code available <a href="<?php echo $githuburl; ?>circle1">here</a>)</li>
+			<li><a href="register/registerform.php">Validate for compulsory fields using Register Form as example</a>  (code available <a href="<?php echo $githuburl; ?>register">here</a>)</li>
+			<li><a href="Ex1-5/index.php">Ex 1-5 (TODO)</a>  (skeleton available <a href="<?php echo $githuburl; ?>Ex1-5">here</a>)</li>			
 		</ul>
 	</body>
 </html>
