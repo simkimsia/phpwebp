80f75ace6ef14bf5301f667dc2879df348919c61
kimsia <kimcity@gmail.com>
2012-05-04 19:44:05 +0800

    Add Ex1-5 folder to lesson5

---
+++ lesson5/Ex1-5/DipList/DipList.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>
+			Temasek Informatics &amp; IT School
+		</title>
+	</head>
+	<body>
+		<table cellpadding="0" cellspacing="0" style="width: 100%; height: 100px;">
+			<tr>
+				<td style="width: 100%; height: 20%;">
+					<h1 style="vertical-align: middle; text-align: center">
+						Welcome to Temasek Informatics &amp; IT School
+					</h1>
+				</td>
+			</tr>
+			<tr>
+				<td style="width: 100%; height: 70%;">
+					Please choose a diploma you are interested in:<br>
+					<ul>
+						<li>
+							<a href="DipListShow.php">Diploma in IT</a>
+						</li>
+						<li>
+							<a href="DipListShow.php">Diploma in Internet and Multimedia Design</a>
+						</li>
+						<li>
+							<a href="DipListShow.php">Diploma in Mobile and Wireless</a>
+						</li>
+						<li>
+							<a href="DipListShow.php">Diploma in Financial Business Informatics</a>
+						</li>
+						<li>
+							<a href="DipListShow.php">Diploma in Cyber and Digital Security</a>
+						</li>
+						<li>
+							<a href="DipListShow.php">Diploma in Gaming and Entertainment Technology</a>
+						</li>
+					</ul>
+				</td>
+			</tr>
+			<tr>
+				<td style="width: 100%; height: 10%;">
+					Copyright <?php echo date('Y'); ?> by Temasek Informatics &amp; IT School. All rights reserved.
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
---
+++ lesson5/Ex1-5/DipList/DipListShow.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>
+			Diploma in Internet and Multimedia Design
+		</title>
+	</head>
+	<body>
+		<table cellpadding="0" cellspacing="0" style="width: 100%; height: 100px;">
+			<tr>
+				<td style="width: 100%; height: 20%;">
+					<h1 style="vertical-align: middle; text-align: center">
+						Welcome to Temasek IT School
+					</h1>
+				</td>
+			</tr>
+			<tr>
+				<td style="width: 100%; height: 70%;">
+					<img src="images/2.jpg" />
+				</td>
+			</tr>
+			<tr>
+				<td style="width: 100%; height: 10%;">
+					Copyright <?php echo date('Y'); ?> by Temasek Informatics &amp; IT School. All rights reserved.
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
