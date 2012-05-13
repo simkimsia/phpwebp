8621b841315ed793bcda23edf57e088725360c9a
kimsia <kimcity@gmail.com>
2012-05-14 07:41:57 +0800

    update the index

--- lesson7/index.php
+++ lesson7/index.php
@@ -22,9 +22,9 @@
 		<h1>Combine result and form into 1: POSTBACK into the same form & STICKY Form</h1>
 		<ul>
 			<li><a href="circle/circleform.php">POSTBACK to same form: Circle</a>  (code available <a href="<?php echo $githuburl; ?>circle">here</a>)</li>
-			<li><a href="square/squareform.php">POSTBACK to same form: Square</a>  (TODO)</li>
+			<li><a href="square/squareform.php">POSTBACK to same form: Square</a>  (TODO) You may refer to (code available <a href="<?php echo $githuburl; ?>square">here</a>)</li>
 			<li><a href="circle1/circle1form.php">STICKY FORM: Circle1</a>  (code available <a href="<?php echo $githuburl; ?>circle1">here</a>)</li>
-			<li><a href="triangle/triangleform.php">STICKY FORM: Triangle</a>  (TODO)</li>
+			<li><a href="triangle/triangleform.php">STICKY FORM: Triangle</a>  (TODO)  You may refer to (code available <a href="<?php echo $githuburl; ?>triangle">here</a>)</li>
 		</ul>
 	</body>
 </html>
