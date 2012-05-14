dbaa303000cbfc73d257e2eeae9ed207cc00d3cc
kimsia <kimcity@gmail.com>
2012-05-14 14:09:43 +0800

    Add new files for lesson8

---
+++ lesson8/index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+	$githuburl = "https://github.com/simkimsia/phpwebp/blob/lesson7/lesson7/";
+
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			Lesson 8 - Redirect when successful
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		<h1>Successful form submission results in redirect - Unsuccessful shows validation in same form</h1>
+		<ul>
+			<li><a href="login/loginform.php">Successful redirect: Login</a>  (code available <a href="<?php echo $githuburl; ?>login">here</a>)</li>
+		
+			<li><a href="Ex1-5/index.php">Fix Register and Login form in Ex1-5</a>  (TODO) </li>
+		</ul>
+	</body>
+</html>
---
+++ lesson8/login/somesillyfile.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			Some silly page you see after successful login
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		You should see this after successful login
+		
+		<br />
+		<br />		
+		<a href="../index.php">Back to index</a>
+	</body>
+</html>
