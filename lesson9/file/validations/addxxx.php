55923bda98ead09d86592fdaf5f102593e833d11
kimsia <kimcity@gmail.com>
2012-06-18 18:06:43 +0800

    Remove unnecessary validations

--- lesson9/file/validations/addxxx.php
+++ lesson9/file/validations/addxxx.php
@@ -6,26 +6,14 @@ $userArriveByDirectlyClickingUrl = !$userArriveBySubmittingAForm;
 
 // check if user arrives here via a POSTBACK	
 if ($userArriveBySubmittingAForm) {		
-/** 		* 		* Part 1 validation		*		***/		
+
 	$subjectNotGiven = empty($_POST['subject']);		
-	// if name not given		
+
 	if ($subjectNotGiven) {			
-		// we add new error into $errors			
-		$errors['subject'] = "subject is required";		
+
+		$errors['subject'] = "Subject is required";		
 	} 
 	
-	$bodyNotGiven = empty($_POST['body']);		
-	// if name not given		
-	if ($bodyNotGiven) {			
-		// we add new error into $errors			
-		$errors['body'] = "Body is required";		
-	} 
-	
-	$interestsNotSelected = empty($_POST['interests']);		// if interest not selected		
-	if ($interestsNotSelected) {			
-		// we add new error into $errors			
-		$errors['interests'] = "Please select at least one interest";		
-	} // end if insteres not selected		
 	
 	require_once('fileupload.php');
 	
@@ -37,22 +25,8 @@ if ($userArriveBySubmittingAForm) {
 	if ($noErrors) {			
 		if (!empty($_POST['subject'])) {				
 			$subject = $_POST['subject'];			
-		} // end if name NOT empty		
+		} // end if subject NOT empty		
 		
-		if (!empty($_POST['body'])) {				
-			$body = $_POST['body'];			
-		} // end if name NOT empty			
-		
-		if (!empty($_POST['nondefault'])) {
-			$nondefaultKeywords = explode(",", $_POST['nondefault']);
-		}
-		
-		if (!empty($_POST['interests']) && is_array($_POST['interests'])) {				
-			$interests = $_POST['interests'];			
-		} // end if hobbies NOT empty
-		
-		// here we combine the two arrays $interests and $nondefaultKeywords into a single array
-		$interests = array_unique(array_merge($interests, $nondefaultKeywords));
 
 		if ($successfullyMoveFile) {
 			$imageSource = $newFileName;
