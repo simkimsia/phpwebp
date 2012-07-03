bd8ee4d21550c477f593f38afbfcd787b8ff1192
kimsia <kimcity@gmail.com>
2012-07-04 00:03:34 +0800

    add in the edit form for studentview

--- lesson10/students/studentview.php
+++ lesson10/students/studentview.php
@@ -1,7 +1,30 @@
 <?php
-	require_once('config/database.php');
 	
 	$studentToDisplay = array();
+	
+	$students = array(
+		'A001' => array(
+			'id'	=> 'A001',
+			'url' 	=> 'studentview.php?id=A001',
+			'name' 	=> 'Charles Xavier',
+			'GPA'	=> 1.5,
+			'image'	=> 'images/xavier.jpg'
+		),
+		'A002' => array(
+			'id'	=> 'A002',
+			'url' 	=> 'studentview.php?id=A002',
+			'name' 	=> 'Erik Magnus Lensherr',
+			'GPA'	=> 2.7,
+			'image'	=> 'images/erik.jpeg'
+		),
+		'A003' => array(
+			'id'	=> 'A003',
+			'url' 	=> 'studentview.php?id=A003',
+			'name' 	=> 'Jean Grey',
+			'GPA'	=> 4.0,
+			'image' => 'images/jean.jpeg'
+		),
+	);
 
 	// we need to know what student id was used to display the details
 	$studentID = $_GET['id']; 
@@ -60,26 +83,28 @@
 		</h1>
 		
 		
-		<form action="studentedit.php" method="post">
+		<form action="studentedit.php" method="post"> <!--  the edit form should contain all the form elements -->
+			
 		<table>
 			<tr>
 				<td><strong>Profile: </strong></td>
 				<td><?php echo $image; ?></td>
 			</tr>
-			<tr>
-				<td><strong>ID: </strong></td>
-				<td><?php echo $id; ?></td>
-			</tr>
+			<!-- this input type is for id . it MUST be invisible to the visitor, so we choose hidden type -->
+			<input type="hidden" name="id" value="<?php echo $id; ?>" />
 			<tr>
 				<td><strong>Name: </strong></td>
-				<td><?php echo $name; ?></td>
+				<td><input type="text" name="name" value="<?php echo $name; ?>" /></td>
 			</tr>
 			<tr>
 				<td><strong>GPA: </strong></td>
-				<td><?php echo $gpa; ?></td>
+				<td><input type="text" name="gpa" value="<?php echo $gpa; ?>" /></td>
 			</tr>									
 		</table>
-		</form>
+			
+			<input type="submit" value="Update!" /><!-- this submit button must be inside the form element -->
+		
+		</form><!-- end of form -->
 		
 		<br />
 		<a href="index.php">Back to index</a><br />
