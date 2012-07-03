fed43e441b0500f8a2013177256fe1e1f35dc1fd
kimsia <kimcity@gmail.com>
2012-07-03 18:32:46 +0800

    Add change for formatting for lesson10 index

--- lesson10/students/index.php
+++ lesson10/students/index.php
@@ -41,13 +41,24 @@
 		<h1>All students</h1>
 		<table>
 			<?php foreach($students as $key => $student) : ?>
+				<?php
+				
+				// here we pick out the values we need
+				$name = $student['name'];
+				$image = $student['image'];
+				$id = $student['id'];
+				$viewUrl = 'studentview.php?id=' . $id;
+				$deleteUrl = 'studentdelete.php?id=' . $id;				
+				
+				?>
+				
 			<tr>
 				<td>
-					<img width="200" height="200" src="<?php echo $student['image']; ?>" /><br />
-					<?php echo $student['name']; ?>
+					<img width="200" height="200" src="<?php echo $image; ?>" /><br />
+					<?php echo $name; ?>
 				</td>
 				<td>
-					<a href="<?php echo $student['url']; ?>">View </a> | <a href="deletestudent.php?id=<?php echo $student['id']; ?>">Delete</a>
+					<a href="<?php echo $viewUrl; ?>">View </a> | <a href="<?php echo $deleteUrl; ?>">Delete</a>
 				</td>
 			</tr>
 			<?php endforeach; ?>
