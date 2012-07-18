7b949b5a31776a6a89e11741ebc254276fd14881
kimsia <kimcity@gmail.com>
2012-07-18 12:05:06 +0800

    Add in the extra step for updating git

---
+++ cloudcontrol/update.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			Welcome to CloudControl docs
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		<h1>Update cloudcontrol using git and cctrlapp</h1>
+		<ol>
+			<li><p>Turn on your git bash or command line prompt</p></li>
+			<li><p>Type <i>cd /c/xampp/htdocs/<strong>YOUR_FOLDER</strong></i>For e.g.,<br />
+				</p></li>
+			<li>
+				<p>Type <i>git add . <strong>(NOTICE THE DOT!!)</strong></i>For e.g.,<br />
+					</p>
+			</li>
+			<li>
+				<p>Type <i>git commit -m 'initial commit'<strong>Fill in any message you want</strong></i>For e.g.,<br />
+					</p>
+			</li>
+			<li>
+				<p>Type <i>cctrlapp <strong>APP_NAME/DEP_NAME</strong> push</i> For e.g.,<br />
+					</p>
+			</li>
+			<li>
+				<p>Type <i>cctrlapp <strong>APP_NAME/DEP_NAME</strong> deploy</i> For e.g.,<br />
+						</p>
+			</li>
+				
+			<li>if you are prompted for your cloudcontrol email and password, type it in.</li>
+			
+		</ol>
+		<a href="mysql/index.php">Back to using mysql in cloudcontrol</a>
+		
+	</body>
+</html>
