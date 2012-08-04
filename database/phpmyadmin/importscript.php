e2c8283dfab5f47c2a46ebdd4b9f87648669bd93
kimsia <kimcity@gmail.com>
2012-08-04 14:12:18 +1000

    add in phpmyadmn files

---
+++ database/phpmyadmin/importscript.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>
+			Import database script using PHPMyAdmin
+		</title>
+		<meta name="author" content="kim sia"><!-- Date: 2012-04-30 -->
+	</head>
+	<body>
+		<h1>Import database script using PHPMyAdmin</h1>
+		<ol>
+			<li>
+				<p>Turn on mysql and apache<br />
+				and click on admin<br />
+				<img src="../images/turnonphpmyadmin.png" /></p>
+			</li>
+			<li>
+				<p>click on import<br /><br />
+				<img src="../images/importinphpmyadmin.png" /></p>
+			</li>
+			<li>
+				<p>Select the sql file you want to import. Press Go.</p>
+			</li>
+			
+			<li>
+				<p>You should see the new database appearing on the lefthand side</p>
+			</li>
+
+		</ol>
+		<a href="../index.php">Back</a>
+	</body>
+</html>
